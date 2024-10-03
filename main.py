568e4f3f6a1d5372498d34e4a5f126184ee32439
Ritu <rn26rituraj@gmail.com>
2024-10-03 23:48:58 +0530

    adding loop for printing odd no

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ if num % 2 == 0:
 else:
     print(f"{num} is a odd number")
 
+
+# even no
 for i in range(2,10,2):
+    print(i,end=",")
+
+# odd no 
+for i in range(1,10,2):
     print(i,end=",")
